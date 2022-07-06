e0f4454896e2ec5ca3527ed46792b0bf67dcd7d3
Louis Lu <louis.lu@westpac.co.nz>
2022-07-07 11:26:08 +1200

    Fix the bug for App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Counter from "./components/Counter";
 import { getInterestRateType, getIsFixedRate } from "./redux/reducers/LendingAccountDetailsReducer";
